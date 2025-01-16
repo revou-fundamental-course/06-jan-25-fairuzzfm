f91c12b83907e071d8b241e8664c64ded1b8e561
fairuzzfm <fairuzzfm@gmail.com>
2025-01-16 16:56:23 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
 /*File js*/
 console.log('Hello World!');
 
-// Fungsi validasi form
-function formValidation() {
-
-    let name = document.getElementById('name-input').value;
-    console.log('Form Validation has been called');
-    if (name == "") {
-        //Kondisi jika name kosong
-        alert('Name must be filled out');
-    }
-    else {
-        //display result name
-        document.getElementById('result-form').innerHTML =  nameInput;
-    }
-
-}
-document.getElementById('submit-btn').addEventListener('click', formValidation);
-
 let indexBanner = 0;
 
 changeBackground();
@@ -47,3 +30,33 @@ function changeBackground() {
 }
 
 setInterval(nextBanner, 3000);
+
+function submitForm() { 
+    const currentTime = new Date().toString(); 
+    const nama = document.getElementById('nama').value; 
+    const tanggalLahir = document.getElementById('tanggal_lahir').value; 
+    const jenisKelamin = document.querySelector('input[name="jenis_kelamin"]:checked').value; 
+    const pesan = document.getElementById('pesan').value; 
+    document.getElementById('currentTime').textContent = currentTime; 
+    document.getElementById('displayNama').textContent = nama; 
+    document.getElementById('displayTanggalLahir').textContent = tanggalLahir; 
+    document.getElementById('displayJenisKelamin').textContent = jenisKelamin; 
+    document.getElementById('displayPesan').textContent = pesan; 
+}
+
+// Fungsi validasi form
+function formValidation() {
+
+    let name = document.getElementById('name-input').value;
+    console.log('Form Validation has been called');
+    if (name == "") {
+        //Kondisi jika name kosong
+        alert('Name must be filled out');
+    }
+    else {
+        //display result name
+        document.getElementById('result-form').innerHTML =  nameInput;
+    }
+
+}
+document.getElementById('submit-btn').addEventListener('click', formValidation);
