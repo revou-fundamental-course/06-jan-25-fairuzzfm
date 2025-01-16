43c9ac73e7618bc081671a7cf658c892998a23c4
fairuzzfm <fairuzzfm@gmail.com>
2025-01-16 19:10:16 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 /*File js*/
 console.log('Hello World!');
 
+// Fungsi validasi form
+// function formValidation() {
+
+//     let name = document.getElementById('name-input').value;
+//     console.log('Form Validation has been called');
+//     if (name == "") {
+//         //Kondisi jika name kosong
+//         alert('Name must be filled out');
+//     }
+//     else {
+//         //display result name
+//         document.getElementById('result-form').innerHTML =  nameInput;
+//     }
+
+// }
+// document.getElementById('submit-btn').addEventListener('click', formValidation);
+//Fungsi untuk mengganti background
 let indexBanner = 0;
 
 changeBackground();
@@ -33,30 +50,13 @@ setInterval(nextBanner, 3000);
 
 function submitForm() { 
     const currentTime = new Date().toString(); 
-    const nama = document.getElementById('nama').value; 
+    const name = document.getElementById('name').value; 
     const tanggalLahir = document.getElementById('tanggal_lahir').value; 
     const jenisKelamin = document.querySelector('input[name="jenis_kelamin"]:checked').value; 
     const pesan = document.getElementById('pesan').value; 
     document.getElementById('currentTime').textContent = currentTime; 
-    document.getElementById('displayNama').textContent = nama; 
+    document.getElementById('displayNama').textContent = name; 
     document.getElementById('displayTanggalLahir').textContent = tanggalLahir; 
     document.getElementById('displayJenisKelamin').textContent = jenisKelamin; 
     document.getElementById('displayPesan').textContent = pesan; 
 }
-
-// Fungsi validasi form
-function formValidation() {
-
-    let name = document.getElementById('name-input').value;
-    console.log('Form Validation has been called');
-    if (name == "") {
-        //Kondisi jika name kosong
-        alert('Name must be filled out');
-    }
-    else {
-        //display result name
-        document.getElementById('result-form').innerHTML =  nameInput;
-    }
-
-}
-document.getElementById('submit-btn').addEventListener('click', formValidation);
